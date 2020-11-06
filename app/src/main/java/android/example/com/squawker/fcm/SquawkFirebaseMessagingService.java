36ac9ce09728a129c5e4f1ab3cd4fca40c6bb2d9
downs990 <downscourtney17@gmail.com>
2020-11-06 14:28:05 -0500

    fixed issue with Schematic, ContentProvider library

--- app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessagingService.java
@@ -2,22 +2,15 @@ package android.example.com.squawker.fcm;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
 import java.util.Map;
 
 public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
 
 
     // TODO (1) Make a new Service in the fcm package that extends from FirebaseMessagingService.
-    // TODO (2) As part of the new Service - Override onMessageReceived. This method will
-    // be triggered whenever a squawk is received. You can get the data from the squawk
-    // message using getData(). When you send a test message, this data will include the
-    // following key/value pairs:
-    // test: true
-    // author: Ex. "TestAccount"
-    // authorKey: Ex. "key_test"
-    // message: Ex. "Hello world"
-    // date: Ex. 1484358455343
+    // TODO (2) As part of the new Service - Override onMessageReceived.
+
+
     // TODO (3) As part of the new Service - If there is message data, get the data using
     // the keys and do two things with it :
     // 1. Display a notification with the first 30 character of the message
@@ -27,6 +20,15 @@ public class SquawkFirebaseMessagingService extends FirebaseMessagingService {
     // look at the notes in the classroom for help.
 
 
+
+    // This method will be triggered whenever a squawk is received. You can get the data from the squawk
+    // message using getData(). When you send a test message, this data will include the
+    // following key/value pairs:
+    // test: true
+    // author: Ex. "TestAccount"
+    // authorKey: Ex. "key_test"
+    // message: Ex. "Hello world"
+    // date: Ex. 1484358455343
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
